8986f15a24defba2eb86ac24184457269eff3c39
st9ho3 <stahos21@gmail.com>
2025-09-03 22:21:24 +0300

    Updating recipe

--- src/app/services/ingredientService.ts
+++ src/app/services/ingredientService.ts
@@ -1,8 +1,8 @@
 import { IIngredientService } from "@/types/services";
 import { IngredientRepository } from "../repositories/ingredientRepository";
-import { Ingredient } from "@/shemas/recipe";
+import { DBRecipe, Ingredient } from "@/shemas/recipe";
 import { checkIfIngredientExists } from "@/db/helpers";
-import { transformIngredientToDB } from "./helpers";
+import { getTotalPrice, transformIngredientFromDB, transformIngredientToDB, transformRecipeIngredentFromDB } from "./helpers";
 import { zodValidateIngredientBeforeAddItToDatabase } from "./services";
 import { RecipeRepository } from "../repositories/recipeRepository";
 
@@ -56,8 +56,20 @@ export class IngredientService implements IIngredientService {
 
           const ingredientId = DBIngredient ? await this.ingredientRepository.update(DBIngredient) : undefined
           const recipes = ingredientId ? await this.recipeRepository.findAllByIngredientId(ingredientId?.ingredientId) : []
+          
 
-          console.log(recipes)
+          if (recipes) {
+
+            for (const recipe of recipes) {
+            const queredRecipe = await this.recipeRepository.findById(recipe.id)
+            const dbIngredients = queredRecipe?.recipeIngredients.map((ingredient) => ingredient)
+            const ingredients = dbIngredients?.map((ing) => transformRecipeIngredentFromDB(ing))
+            const cost = ingredients ? getTotalPrice(ingredients) : 0
+            const tax = recipe.tax
+            const price = recipe.sellingPrice
+            }
+          } 
+          
           return ingredientId
         } catch(err) {
             console.log(err)
