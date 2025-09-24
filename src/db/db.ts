f7a85eaad9a9ffbb1e4c3a342542b6c8dbc23a58
st9ho3 <stahos21@gmail.com>
2025-09-24 18:35:33 +0300

    docs/remove db docs

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,11 +3,8 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema';
 
-// Make sure your DATABASE_URL from Neon is the "Pooled" connection string
-// It should look like: postgresql://user:pass@host/db?sslmode=require
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
 
-// This driver supports transactions
 export const db = drizzle(pool, { schema });
